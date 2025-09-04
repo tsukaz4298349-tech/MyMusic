b3e6a3a64984bb535f40d588a3736406674ab618
中尾司 <tsukaz4298349@gmail.com>
2025-09-04 22:53:35 +0900

    Initial Commit

---
+++ MyMusic/MyMusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMusicApp.swift
+//  MyMusic
+//
+//  Created by 中尾司 on 2025/09/04.
+//
+
+import SwiftUI
+
+@main
+struct MyMusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
